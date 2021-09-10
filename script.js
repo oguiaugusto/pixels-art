633c25f53ec55fc9eef685fb5e2d70b51cc2da0f
gui <guilhaugusto05@gmail.com>
2021-09-10 15:10:16 -0300

    Requisitos 2 e 3

---
+++ script.js
@@ -0,0 +1,18 @@
+// Definir cores da Paleta
+const pallete = document.querySelectorAll('.color');
+const firstPColor = pallete[0];
+const secondPColor = pallete[1];
+const thirdPColor = pallete[2];
+const forthPColor = pallete[3];
+
+firstPColor.style.color = 'black';
+firstPColor.style.backgroundColor = 'black';
+
+secondPColor.style.color = '#4f58b9';
+secondPColor.style.backgroundColor = '#4f58b9';
+
+thirdPColor.style.color = '#d14a4a';
+thirdPColor.style.backgroundColor = '#d14a4a';
+
+forthPColor.style.color = '#e6c936';
+forthPColor.style.backgroundColor = '#e6c936';
